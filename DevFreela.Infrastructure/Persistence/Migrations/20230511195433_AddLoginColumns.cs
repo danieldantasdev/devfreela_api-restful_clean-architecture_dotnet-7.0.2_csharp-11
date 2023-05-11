0de02e817d8353f863edeb3130d785bd50bda3ab
danieldantasdev <contatolobinhodev@gmail.com>
2023-05-11 19:33:22 -0300

    feat: add new migrations

---
+++ DevFreela.Infrastructure/Persistence/Migrations/20230511195433_AddLoginColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevFreela.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "user",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "user",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "user");
+        }
+    }
+}
